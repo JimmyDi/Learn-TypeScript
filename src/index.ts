98f592c43b096c4c7ba999ff33d048e00c211754
liudi25ece@outlook.com <liudi25ece@outlook.com>
2022-06-15 07:26:25 +0800

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import './enum';
-import './interface';
+// import './interface';
 // import './datatype';
+import './function';
 
 let hello: string = 'Hello TypeScript';
 document.querySelectorAll('.app')[0].innerHTML = hello;
