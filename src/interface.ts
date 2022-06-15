5ee9acf82b75b8cb803f02c3c9c9d9c2c4a719c2
liudi25ece@outlook.com <liudi25ece@outlook.com>
2022-06-16 07:31:29 +0800

    Interface and class

--- src/interface.ts
+++ src/interface.ts
@@ -67,3 +67,60 @@ lib1();
 lib1.doSomething();
 
 let lib2 = getLib();
+
+interface Human {
+    // new (name: string): void
+    name: string;
+    eat(): void;
+}
+
+class Asian implements Human {
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    name: string;
+    eat() {}
+    sleep() {}
+}
+
+interface Man extends Human {
+    run(): void
+}
+
+interface Child {
+    cry(): void
+}
+
+interface Boy extends Man, Child {
+
+}
+
+let boy: Boy = {
+    name: '',
+    run() {},
+    eat() {},
+    cry() {}
+}
+
+class Auto {
+    state = 1;
+    private state2 = 0;
+
+    // setState2(newState2: number) {
+    //     this.state2 = newState2;
+    // }
+}
+
+interface AutoInterface extends Auto {
+
+}
+
+// class C implements AutoInterface {
+//     state = 1;
+//     // private state2 = 4;
+// }
+
+class Bus extends Auto implements AutoInterface {
+    
+}
