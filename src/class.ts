c16ba2f8eebea3101e9943cd72c1d1eb7d81d35d
liudi25ece@outlook.com <liudi25ece@outlook.com>
2022-06-19 07:56:16 +0800

    union type and intersection type

--- src/class.ts
+++ src/class.ts
@@ -8,57 +8,57 @@ abstract class Animal {
 
 // let animal = new Animal();
 
-class Dog extends Animal {
-    constructor(name: string) {
-        super();
-        this.name = name;
-    }
+// class Dog extends Animal {
+//     constructor(name: string) {
+//         super();
+//         this.name = name;
+//     }
 
-    public name?: string;
-    run() {};
-    private pri() {}
-    protected pro() {}
-    readonly legs: number = 4;
-    static food: string = 'bones';
-    sleep(): void {
-        console.log('dog sleep');
-    }
-}
+//     public name?: string;
+//     run() {};
+//     private pri() {}
+//     protected pro() {}
+//     readonly legs: number = 4;
+//     static food: string = 'bones';
+//     sleep(): void {
+//         console.log('dog sleep');
+//     }
+// }
 
-console.log(Dog.prototype);
-let dog = new Dog('wangwang');
-console.log(dog);
-// dog.pri();
-// dog.pro()
-console.log(Dog.food);
-dog.eat();
-dog.sleep();
+// console.log(Dog.prototype);
+// let dog = new Dog('wangwang');
+// console.log(dog);
+// // dog.pri();
+// // dog.pro()
+// console.log(Dog.food);
+// dog.eat();
+// dog.sleep();
 
-class Husky extends Dog {
-    constructor(name: string, public color: string) {
-        super(name);
-        this.color = color;
-        // this.pri();
-        this.pro();
-    }
+// class Husky extends Dog {
+//     constructor(name: string, public color: string) {
+//         super(name);
+//         this.color = color;
+//         // this.pri();
+//         this.pro();
+//     }
 
-    // color: string;
-}
+//     // color: string;
+// }
 
-console.log(Husky.food);
+// console.log(Husky.food);
 
-class Cat extends Animal {
-    sleep(): void {
-        console.log('Cat sleep');
-    }
-}
+// class Cat extends Animal {
+//     sleep(): void {
+//         console.log('Cat sleep');
+//     }
+// }
 
-let cat = new Cat();
+// let cat = new Cat();
 
-let animals: Animal[] = [dog, cat];
-animals.forEach(i => {
-    i.sleep();
-});
+// let animals: Animal[] = [dog, cat];
+// animals.forEach(i => {
+//     i.sleep();
+// });
 
 class WorkFlow {
     step1() {
