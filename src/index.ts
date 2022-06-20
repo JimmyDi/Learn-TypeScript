ef7de928972ab173896832150702c96c1a46ae40
liudi25ece@outlook.com <liudi25ece@outlook.com>
2022-06-21 07:47:13 +0800

    declaration file

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
 // import './advanced';
 // import './es6/c';
 // import './node/c.node';
+import '../src/libs';
 
 let hello: string = 'Hello TypeScript';
 document.querySelectorAll('.app')[0].innerHTML = hello;
